785e71fa065cf13a9d812b76fc4c511ea31f71f8
Sujan Ghosh <75779154+sfc-gh-sghosh@users.noreply.github.com>
2025-06-19 22:31:13 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from snowflake.snowpark import Session
+from snowflake.snowpark import Session # Session is imported here
 from snowflake.snowpark.types import StringType
 from snowflake.snowpark.functions import sproc
 
@@ -7,5 +7,6 @@ from snowflake.snowpark.functions import sproc
     return_type=StringType(),
     packages=["snowflake-snowpark-python"]
 )
-def hello_from_snowpark(session: "Session") -> "str":
+# FIX: Remove quotes from type hints
+def hello_from_snowpark(session: Session) -> str: # No quotes around Session, no quotes around str
     return f"Hello from Snowpark!"
