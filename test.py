6e19487d645f8d57a55bd5f02247eabec78c344d
Sujan Ghosh <75779154+sfc-gh-sghosh@users.noreply.github.com>
2025-06-19 23:12:00 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-from snowflake.snowpark import session
+from snowflake.snowpark.session import Session
 from snowflake.snowpark.types import StringType
 from snowflake.snowpark.functions import sproc
 
 @sproc(
-    session=session,
     name="hello_from_snowpark",
     return_type=StringType(),
+    replace="true",
     packages=["snowflake-snowpark-python"]
 )
 # FIX: Remove quotes from type hints
-def hello_from_snowpark(session: session) -> str: 
+def hello_from_snowpark(session: Session) -> str: 
     return f"Hello from Snowpark!"
+
+result = session.call("hello_from_snowpark")
+print(result)
