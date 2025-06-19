8bceb2bd9d4971ac6275cf5871e73b29a54c2a28
Sujan Ghosh <75779154+sfc-gh-sghosh@users.noreply.github.com>
2025-06-19 23:32:29 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.types import StringType
 from snowflake.snowpark.functions import sproc
 
+
 @sproc(
     name="hello_from_snowpark",
     return_type=StringType(),
+    input_types=[],  
     replace=True,
     packages=["snowflake-snowpark-python"]
 )
-def hello_from_snowpark(session: "Session") -> str:
+def hello_from_snowpark(session):
     return "Hello from Snowpark!"
